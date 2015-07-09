ac7e38ba70cb1a0070973ed50ec2e15707a4d76b
Matthew Gray <matthew.gray@powershop.co.nz>
2015-07-09 18:38:57 +1200

    Feature: Add computed property #ready? to Game

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  # TODO
+  let(:game) { Game.new }
+  let(:player1) { Player.new }
+  let(:player2) { Player.new }
+
+  context("when fewer than 2 players") do
+    before { game.players << player1 }
+
+    it "is not #ready? to start" do
+      expect(game).to_not be_ready
+    end
+  end
+
+  context("with 2 players") do
+    before do
+      game.players << player1
+      game.players << player2
+    end
+
+    it "is #ready? to start" do
+      expect(game).to be_ready
+    end
+  end
 end
