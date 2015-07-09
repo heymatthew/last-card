e0b28a18e22b1e43245d60fafc2e55f3476685ac
Matthew Gray <matthew.gray@powershop.co.nz>
2015-07-10 11:05:03 +1200

    Feature: Setup validation on Actions

--- app/models/action.rb
+++ app/models/action.rb
@@ -2,4 +2,8 @@ class Action < ActiveRecord::Base
   has_one :card
   has_one :player
   belongs_to :game
+
+  validates :card, presence: true
+  validates :player, presence: true
+  validates :game, presence: true
 end
