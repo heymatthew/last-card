dbca4c485fceca542020e3e760696c3275106a6b
Matthew B. Gray <himself@matthew.geek.nz>
2015-07-10 21:42:49 +1200

    Feature: Setup card equality and hashing methods

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,8 +2,17 @@ class Card < ActiveRecord::Base
   belongs_to :action
 
   # TODO notes, Ruby makes these available as constants!
+  # TODO what does freeze do?
   RANKS = %w( 2 3 4 5 6 7 8 9 10 jack queen king ace ).freeze
   SUITS = %w( hearts spades dimons clubs ).freeze
+
+  def ==(your)
+    self.suit == your.suit && self.rank == your.rank
+  end
+
+  def hash
+    [suit, rank].hash
+  end
 
   validates :rank,
     presence: true,
