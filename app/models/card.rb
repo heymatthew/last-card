899e5e738476e6e6d1c4d71e726f36c577d39742
Matthew B. Gray <code@matthew.nz>
2015-07-14 15:00:45 +1200

    Fix: Cascade delete actions when deleting card or game

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,5 @@
 class Card < ActiveRecord::Base
-  has_many :actions
+  has_many :actions, :dependent => :delete_all
 
   RANKS = %w( 2 3 4 5 6 7 8 9 10 jack queen king ace ).freeze
   SUITS = %w( hearts spades dimonds clubs ).freeze
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ActiveRecord::Base
   # TODO has_and_belongs_to_many players
   has_many :players
-  has_many :actions
+  has_many :actions, :dependent => :delete_all
 
   def ready?
     players.size >= 2
