40f197378c6ce7c0101586e9e16efa3ffb595f5b
Matthew Gray <matthew.gray@powershop.co.nz>
2015-07-15 09:29:56 +1200

    Remove rendundant validations

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,14 +4,8 @@ class Card < ActiveRecord::Base
   RANKS = %w( 2 3 4 5 6 7 8 9 10 jack queen king ace ).freeze
   SUITS = %w( hearts spades dimonds clubs ).freeze
 
-  validates :rank,
-    presence: true,
-    inclusion: { in: RANKS }
-
-  validates :suit,
-    presence: true,
-    inclusion: { in: SUITS }
-
+  validates :rank, inclusion: { in: RANKS }
+  validates :suit, inclusion: { in: SUITS }
   validates :suit, uniqueness: {scope: :rank}
 
   def self.deck
