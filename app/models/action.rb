3c96fad0d8912a6832ba39ed6be30cfd3c9aaeb8
Matthew Gray <matthew.gray@powershop.co.nz>
2015-07-22 16:45:20 +1200

    Refactor explicit action constants for affects

--- app/models/action.rb
+++ app/models/action.rb
@@ -3,7 +3,8 @@ class Action < ActiveRecord::Base
   belongs_to :player
   belongs_to :game
 
-  PICKUP, PLAY = (0..5).to_a
+  PICKUP = 0
+  PLAY = 1
 
   validates :card,   presence: true
   validates :player, presence: true
