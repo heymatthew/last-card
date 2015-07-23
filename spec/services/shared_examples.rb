44d1f7b66240592127e3c660e723665b761d3d52
Matthew B. Gray <code@matthew.nz>
2015-07-23 15:46:32 +1200

    Refactor pull up shared examples for services with errors
    Move "a service with errors" behaviour into a shared_examples file so it
    can be included in future tests.

---
+++ spec/services/shared_examples.rb
@@ -0,0 +1,12 @@
+# To use this, just include it :)
+RSpec.shared_examples "a service with errors" do
+  context "when called" do
+    it "bails" do
+      expect(service.call).to be false
+    end
+
+    it "gives errors" do
+      expect { service.call }.to change { service.errors.size }.by(1)
+    end
+  end
+end
