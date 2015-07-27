bbde04a76560134641223724e78e6af2885c30c7
Matthew B. Gray <code@matthew.nz>
2015-07-28 09:56:45 +1200

    Remove dependence on Action model from migration

--- db/migrate/20150714024130_drop_invalid_actions.rb
+++ db/migrate/20150714024130_drop_invalid_actions.rb
@@ -1,7 +1,18 @@
 class DropInvalidActions < ActiveRecord::Migration
   def up
-    Action.where(card:nil).each(&:destroy)
-    Action.where(game:nil).each(&:destroy)
-    Action.where(player:nil).each(&:destroy)
+    #Action.where(card:nil).each(&:destroy)
+    #Action.where(game:nil).each(&:destroy)
+    #Action.where(player:nil).each(&:destroy)
+
+    execute <<-ENDSQL
+      DELETE FROM actions
+      WHERE card_id IS NULL
+         OR game_id IS NULL
+         OR player_id IS NULL
+    ENDSQL
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
