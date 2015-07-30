e30511dc7013ddec412d9da8ef568291b0b7a146
Matthew Gray <matthew.gray@powershop.co.nz>
2015-07-30 12:26:07 +1200

    Delete cards with SQL rather than models (stops things from 'splodin)

--- db/migrate/20150710100204_create_card_deck.rb
+++ db/migrate/20150710100204_create_card_deck.rb
@@ -1,12 +1,25 @@
 class CreateCardDeck < ActiveRecord::Migration
+  include ActiveRecord::Sanitization::ClassMethods
+
+  RANKS = %w( 2 3 4 5 6 7 8 9 10 jack queen king ace ).freeze
+  SUITS = %w( hearts spades dimonds clubs ).freeze
+  INSERT_CARD = <<-ENDSQL
+    INSERT INTO cards(rank, suit, created_at, updated_at)
+    VALUES(?, ?, ?, ?)
+  ENDSQL
+
   def up
-    Card.destroy_all
-    Card::RANKS.product(Card::SUITS).map do |rank, suit|
-      Card.create!(rank: rank, suit: suit)
+    execute "DELETE FROM cards"
+
+    now = Time.now
+    RANKS.product(SUITS).map do |rank, suit|
+      execute sanitize_sql_array([
+        INSERT_CARD, rank, suit, now, now
+      ])
     end
   end
 
   def down
-    Card.destroy_all
+    raise ActiveRecord::IrreversibleMigration
   end
 end
