8cc4851bf4ec9dc1b9598bdf6118a7a3ea90f094
Matthew Gray <matthew.gray@powershop.co.nz>
2015-08-03 12:32:25 +1200

    Add pickups to pile concept

--- spec/concepts/pile_spec.rb
+++ spec/concepts/pile_spec.rb
@@ -12,4 +12,29 @@ RSpec.describe Pile do
   it "shows the last card on top" do
     expect(pile.top).to be cards.last
   end
+
+  context "when no pickups on top of deck" do
+    it "has 0 pickups" do
+      expect(pile.pickups).to be 0
+    end
+  end
+
+  context "when a 5 is on the top of the pile" do
+    before { pile.push(Card.new("5", "dimonds")) }
+
+    it "has 5 pickups" do
+      expect(pile.pickups).to be 5
+    end
+  end
+
+  context "when a 2 is played on a 5" do
+    before do
+      pile.push(Card.new("5", "dimonds"))
+      pile.push(Card.new("2", "dimonds"))
+    end
+
+    it "has 7 pickups" do
+      expect(pile.pickups).to be 7
+    end
+  end
 end
