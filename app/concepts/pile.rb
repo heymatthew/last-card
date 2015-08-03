ca90ab0d30d1b481a35c45acc2c91fbcbb9360ab
Matthew B. Gray <code@matthew.nz>
2015-08-03 12:32:25 +1200

    Add pickups to pile concept

--- app/concepts/pile.rb
+++ app/concepts/pile.rb
@@ -3,5 +3,16 @@ class Pile < Array
     last
   end
 
-  # TODO count_pickups
+  def pickups
+    0 if top.pickup.nil?
+
+    pickup_count = 0
+    cards = self.clone
+    while card = cards.pop
+      break if card.pickup.nil?
+      pickup_count += card.pickup
+    end
+
+    pickup_count
+  end
 end
