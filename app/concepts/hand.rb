fa16f94b4a12181eff129f94a7da32c63b1bc5dc
Matthew B. Gray <code@matthew.nz>
2015-08-05 20:57:18 +1200

    Prune back unused methods

--- app/concepts/hand.rb
+++ app/concepts/hand.rb
@@ -1,12 +1,4 @@
 class Hand < Array
-  def filter_suit(suit)
-    select_from_hand { |card| card.suit == suit }
-  end
-
-  def filter_rank(rank)
-    select_from_hand { |card| card.rank == rank }
-  end
-
   def select_playable(top_card)
     select_from_hand { |card| card.playable_on?(top_card) }
   end
