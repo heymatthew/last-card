923a4eba02fc35dd47451a03b42fe4e43dfd0cae
Matthew Gray <matthew.gray@powershop.co.nz>
2015-08-13 15:09:39 +1200

    Refactor PlayCards into a struct for simplicity

--- app/services/play_cards.rb
+++ app/services/play_cards.rb
@@ -1,15 +1,10 @@
-class PlayCards
-  attr_reader :errors
-
-  def initialize(player, round, card)
-    @player = player
-    @round  = round
-    @card   = card
-    @errors = []
+class PlayCards < Struct.new(:player, :round, :card)
+  def errors
+    @errors ||= []
   end
 
   def call
-    @round.game.with_lock do
+    round.game.with_lock do
       check_legal_move
 
       play_card! && increment_round! if errors.none?
@@ -22,18 +17,18 @@ class PlayCards
 
   # TODO push out to player_options class and use a validate method
   def check_legal_move
-    top_card = @round.pile.top
-    if !@card.playable_on?(top_card)
-      errors.push "cannot play card, #{@card} on #{top_card}"
+    top_card = round.pile.top
+    if !card.playable_on?(top_card)
+      errors.push "cannot play card, #{card} on #{top_card}"
     end
   end
 
   def play_card!
-    @player.play!(@card)
+    player.play!(card)
   end
 
   def increment_round!
-    @round.game.round_counter += 1
-    @round.game.save!
+    round.game.round_counter += 1
+    round.game.save!
   end
 end
