3914061335a630370df3d9fab897692e80a08e04
Matthew B. Gray <code@matthew.nz>
2015-08-17 16:32:08 +1200

    Fan out cards, hack hack hack

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -1,38 +1,61 @@
+/* global $, d3 */
+/* eslint-disable no-console */
+
 $(document).ready(function() {
-  var url = $('#table').data('deck')
+  function cardKey(card) {
+    return [card.rank, card.suit].join(',');
+  }
 
-  var lookup = {
-    'ace of clubs': {
-      'identifier': '#g196926',
-      'correction': 'translate(600,-110)'
-    },
-    'eight of clubs': {
-      'identifier': '#g196820',
-      'correction': 'translate(600,-110)'
-    },
-  };
+  function randomColor() {
+    return '#'+((1<<24)*Math.random()|0).toString(16);
+  }
 
-  $.get( url ).then( function(deck) {
-    var $deck = $(deck);
+  function rotateCardTransform(count) {
+    var middle = parseFloat(count, 10) / 2;
+    console.log(parseFloat(count));
+    var baseRotation = 20.0; // degrees
+    return function rotateCard(card, i) {
+      var angle = (middle - i) * baseRotation;
+      console.log(middle, i, middle - i, baseRotation, angle);
+      return 'rotate(' + angle + ',50,100)';
+    };
+  }
 
-    function definitions() {
-      return $deck.find('defs')[0];
-    }
+  var svg = d3.select('#table')
+    .append('svg')
+    .attr('height', '100%')
+    .attr('width', '100%')
+  ;
 
-    function card(name) {
-      var details = lookup[name];
-      var $card = $deck.find(details.identifier);
-      $card.attr('transform', details.correction);
+  var hand =
+    svg.append('g')
+      .attr('transform', 'translate(200,500)')
+      .classed('hand', true)
+  ;
 
-      return function() { return $card[0]; }
-    }
 
-    var svg = d3.select("#table").append("svg")
-      .attr("width", "100%")
-      .attr("height", "100%")
-    ;
+  d3.json(document.location + '/rounds')
+    .header('Content-Type', 'application/json')
+    .get(function(error, roundData) {
+      if(error) {
+        return console.error(error);
+      }
 
-    svg.append( card('eight of clubs') )
-    svg.append( definitions )
-  });
+      var handData = roundData.hands['megatron']; // FIXME placeholder
+      var handCards = hand.selectAll('.card').data(handData, cardKey);
+
+      handCards.enter()
+        .append('rect')
+        .attr('height', '150')
+        .attr('width', '100')
+        .attr('fill', randomColor)
+        .attr('y', '-20%')
+        .attr('transform', rotateCardTransform(handData.length))
+      ;
+
+      // Cards that have left hand...
+      //hand.transform('translate(50%,50%)');
+      console.log(handCards);
+    })
+  ;
 });
