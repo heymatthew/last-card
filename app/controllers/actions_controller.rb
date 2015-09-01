d7e38a78508a1a61b6dc0925bafde3f9bff5d2d6
Matthew B. Gray <code@matthew.nz>
2015-09-02 10:41:38 +1200

    Game acitons index returns full blown json

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,6 +1,12 @@
 class ActionsController < ApplicationController
   def index
-    last_time = index_params.since
+    render(json: actions.in_order)
+  end
+
+  private
+
+  def actions
+    last_time = index_params[:since]
     if last_time
       game.actions.since(last_time)
     else
@@ -8,14 +14,11 @@ class ActionsController < ApplicationController
     end
   end
 
-  private
-
-  def game
-    @game ||= Game.find(params[:id])
-  end
-
   def index_params
     params.permit(:since)
   end
 
+  def game
+    @game ||= Game.find(params[:game_id])
+  end
 end
