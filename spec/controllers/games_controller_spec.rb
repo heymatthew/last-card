6a4536e6b31c242ad39be08b7a22a78c5c7593e5
Matthew B. Gray <code@matthew.nz>
2015-09-02 10:57:24 +1200

    Refactor ordering of GameController specs for reusability

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 RSpec.describe GamesController, type: :controller do
   render_views # needed for response.body tests
 
-  describe "GET new" do
-    before do
-      megatron = User.create!(email: "megatron@decepticons.com")
-      User.create!(email: "optimus@autobots.com")
-      session[:user_id] = megatron.id
-    end
+  # User logged in is megatron
+  let(:megatron) { User.create!(email: "megatron@decepticons.com") }
+  before { session[:user_id] = megatron.id }
 
+
+  describe "GET new" do
     it "creates a game after called" do
       expect { get :new }.to change { Game.all.count }.by 1
     end
@@ -17,49 +16,6 @@ RSpec.describe GamesController, type: :controller do
     it "redirects to game after called" do
       get :new
       expect(response).to redirect_to(Game.first)
-    end
-  end
-
-  describe "GET show" do
-    it "explodes with negative game id" do
-      expect { get :show, id: -1 }.to raise_error ActiveRecord::RecordNotFound
-    end
-
-    it "explodes with alpha game id" do
-      expect { get :show, id: "fu" }.to raise_error ActiveRecord::RecordNotFound
-    end
-
-    it "explodes with non existant game id" do
-      expect { get :show, id: 666 }.to raise_error ActiveRecord::RecordNotFound
-    end
-
-    context "a game in progress" do
-      let(:game)     { Game.create! }
-      let(:megatron) { User.create!(email: "megatron@decepticons.com") }
-      let(:optimus)  { User.create!(email: "optimus@autobots.com") }
-
-      subject { get :show, id: game.id }
-      before { session[:user_id] = megatron.id }
-
-      it "can find a game in progress" do
-        subject
-        expect(response).to render_template('show')
-      end
-
-      it "publishes a JOIN action" do
-        expect { subject }
-          .to change { Action.where(effect: Action::JOIN).count }
-          .by 1
-      end
-
-      it "created a new player" do
-        expect { subject }.to change { Player.count }.by 1
-      end
-
-      it "doesn't create a player on page refresh" do
-        subject
-        expect { subject }.to_not change { Player.count }
-      end
     end
   end
 
@@ -86,30 +42,73 @@ RSpec.describe GamesController, type: :controller do
     end
   end
 
-  describe "PUT update" do
-    # TODO
-    # let(:game) { Game.create! }
-    # let(:user1) { User.create!(email: "a@a.a") }
-    # let(:user2) { User.create!(email: "b@b.b") }
+  context "when joining a game" do
+    let(:game) { Game.create! }
 
-    # before { game.players.create!(user: user1) }
+    context "GET show invalid game ID" do
+      it "explodes with negative game id" do
+        expect { get :show, id: -1 }.to raise_error ActiveRecord::RecordNotFound
+      end
 
-    # context "when there is only one player" do
-    #   it "doesn't start the game" do
-    #     expect().to raise
-    #   end
-    # end
+      it "explodes with alpha game id" do
+        expect { get :show, id: "fu" }.to raise_error ActiveRecord::RecordNotFound
+      end
 
-    # context "when there are 2 players" do
-    #   before { game.players.create!(user: user2) }
+      it "explodes with non existant game id" do
+        expect { get :show, id: 666 }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
 
-    #   context "where one player is ready" do
-    #     it ""
-    #   end
+    describe "GET show" do
+      subject { get :show, id: game.id }
 
-    #   context "when both players are ready" do
-    #     it "starts the game"
-    #   end
-    # end
+      it "can find a game in progress" do
+        subject
+        expect(response).to render_template('show')
+      end
+
+      it "publishes a JOIN action" do
+        expect { subject }
+          .to change { Action.where(effect: Action::JOIN).count }
+          .by 1
+      end
+
+      it "created a new player" do
+        expect { subject }.to change { Player.count }.by 1
+      end
+
+      it "doesn't create a player on page refresh" do
+        subject
+        expect { subject }.to_not change { Player.count }
+      end
+    end
+
+    describe "PUT update" do
+      # let(:game) { Game.create! }
+      # let(:user1) { User.create!(email: "a@a.a") }
+      # let(:user2) { User.create!(email: "b@b.b") }
+
+      # before do
+      #   game.players.create!(user: user1)
+      # end
+
+      # context "when there is only one player" do
+      #   it "doesn't start the game" do
+      #     expect().to raise
+      #   end
+      # end
+
+      # context "when there are 2 players" do
+      #   before { game.players.create!(user: user2) }
+
+      #   context "where one player is ready" do
+      #     it ""
+      #   end
+
+      #   context "when both players are ready" do
+      #     it "starts the game"
+      #   end
+      # end
+    end
   end
 end
