207535b7dda2151682f79ce7b96c4a358767ff7f
Matthew B. Gray <code@matthew.nz>
2015-09-02 09:58:57 +1200

    Refactor rename SessionsController#info to #oauth_info for clarity

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,14 +13,14 @@ class SessionsController < ApplicationController
   private
 
   def user
-    User.find_or_create_by(email: info.email) do |new_user|
-      new_user.firstname = info.first_name
-      new_user.name = info.name
-      new_user.image = info.image
+    User.find_or_create_by(email: oauth_info.email) do |new_user|
+      new_user.firstname = oauth_info.first_name
+      new_user.name = oauth_info.name
+      new_user.image = oauth_info.image
     end
   end
 
-  def info
+  def oauth_info
     request.env["omniauth.auth"].info
   end
 end
