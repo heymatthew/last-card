274b1af000b8b45d3729c4f3db832859e1a85449
Matthew B. Gray <code@matthew.nz>
2015-09-04 10:55:30 +1200

    Flesh out state controller spec

--- spec/controllers/states_controller_spec.rb
+++ spec/controllers/states_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe StatesController, type: :controller do
+  let(:game) { Game.create! }
+  let(:megatron) { User.create!(email: "megatron@decepticons.com") }
+  let(:optimus) { User.create!(email: "optimus@prime.net") }
 
+  before do
+    session[:user_id] = megatron.id
+    game.players.create!(user: megatron)
+    game.players.create!(user: optimus)
+  end
+
+  describe "#show" do
+    subject { get :show }
+
+    context "when game has been setup" do
+      # it "lists the players"
+      # it "lists the player"
+      # it "lists oponents"
+    end
+  end
 end
