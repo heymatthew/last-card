56a7dadd08fcd3ee51f647f03a379cab834d8716
Matthew B. Gray <code@matthew.nz>
2015-09-09 08:34:49 +1200

    Mutliline for readability

--- app/concepts/deck.rb
+++ app/concepts/deck.rb
@@ -1,6 +1,9 @@
 class Deck < Array
   # Traditional full 52 card deck
-  PLATONIC = Card::RANKS.product(Card::SUITS).map { |rank, suit| Card.new(rank, suit) }.freeze
+  PLATONIC = Card::RANKS
+    .product(Card::SUITS)
+    .map { |rank, suit| Card.new(rank, suit) }
+    .freeze
 
   def pickup
     shuffle_offset = rand(length)
